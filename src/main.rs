9f7cd0cfe58fc4e9473bf8f1d7f6202d2fbe708d
sendo-kakeru <sendokakeru.js@gmail.com>
2025-03-12 23:24:16 +0900

    feat: Read file name

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,4 +10,12 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("ファイルがありません");
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)
+    .expect("ファイルの読み込みに失敗しました。");
+
+    println!("With text:\n{}", contents);
 }
