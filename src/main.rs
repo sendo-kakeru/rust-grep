1c63781cea0e4b141b8dd0edfc76cd0bb6118cef
sendo-kakeru <sendokakeru.js@gmail.com>
2025-03-13 19:12:25 +0900

    feat: Output log

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn main() {
 
     let config = rust_grep::Config::new(&args).unwrap_or_else(|err| {
         
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = rust_grep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
 
         process::exit(1);
     }
